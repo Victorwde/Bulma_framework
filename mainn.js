7062807695c928bc8eba5e6e1c28e2a73390e311
Victor Souza <victor_fortinho@hotmail.com>
2022-10-01 21:39:35 -0300

    Bulma

---
+++ mainn.js
@@ -0,0 +1,9 @@
+var date = new Date(); 
+result = date.toDateString();
+console.log(result);
+
+var date = new Date(); 
+result = date.toUTCString(); 
+console.log(result);
+
+//Estudar o conceito de tecnologias de desenvolvimento de software
